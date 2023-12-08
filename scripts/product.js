97ee462f9c917bfe986a181091c227044a1b772a
Adri <a.fernandezrodriguez2003@gmail.com>
2023-12-08 15:21:38 +0100

    Creamos el js de productos

---
+++ scripts/product.js
@@ -0,0 +1,56 @@
+
+export class Product {
+    #id;
+    #nombre;
+    #cantidad;
+    #precio;
+
+    constructor(id, nombre , cantidad, precio){
+        this.#id = id;
+        this.#nombre = nombre;
+        this.#cantidad = cantidad;
+        this.#precio = precio;
+    }
+
+    toJSON() {
+        return {
+            id: this.#id,
+            nombre: this.#nombre,
+            cantidad: this.#cantidad,
+            precio: this.#precio
+        };
+    }
+
+    get id(){
+        return this.#id;
+    }
+    set id (id){
+        this.#id = id;
+    }
+    
+    get nombre(){
+        return this.#nombre;
+    }
+    set nombre (nombre){
+        this.#nombre = nombre;
+    }
+
+    get cantidad(){
+        return this.#cantidad;
+    }
+    set cantidad (cantidad){
+        this.#cantidad = cantidad;
+    }
+
+    get precio(){
+        return this.#precio;
+    }
+    set precio (precio){
+        this.#precio = precio;
+    }
+
+    showProduct(){
+        console.log(`Id: ${this.#id}, Nombre: ${this.#nombre}, Cantidad: ${this.#cantidad}, Precio: ${this.#precio}`)
+    }
+
+}
